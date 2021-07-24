1fcabb450815cafa191e0ae009d200dd474bc1f0
Partha <github@partha51613.com>
2021-07-24 21:19:58 +0530

    c8v4 OnSearchSubmit will be refactored

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import axios from "axios";
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos',{
+      params: {query: term},
+      headers: {
+        Authorization : 'Client-ID ftIHjZPAM3MlWZgx0TJSu7LrCUd3C7CaYYcJMQCiXwg'
+      }
+    }).then((response)=>{
+      console.log(response.data.results);
+    });
   }
 
   render() {
