655edd3886e910811e72ef7734acfc47da725ccf
Partha <github@partha51613.com>
2021-09-01 12:43:35 +0530

    Https

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 
+// var cors = require('cors')
+// var app;
+// app.use(cors()) // Use this after the variable declaration
+
 ReactDOM.render(<App />, document.querySelector('#root'));
