0268850b8bf80df68099d5882511146635e0749f
Partha <github@partha51613.com>
2021-09-07 22:31:14 +0530

    chapter 10 video 7 finished

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,34 +1,35 @@
-import React from  "react";
+import React from 'react';
 
 class ImageCard extends React.Component {
-    
+  constructor(props) {
+    super(props);
 
-    constructor(props){
-        super(props);
-        this.imageRef = React.createRef();
+    this.state = { spans: 0 };
 
-       }
+    this.imageRef = React.createRef();
+  }
 
-    componentDidMount(){
-        this.imageRef.current.addEventListener('load', this.setSpans);
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
 
-        
-    }
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
 
-    setSpans = () => {
-        console.log(this.imageRef.current.clientHeight);
-    }
-    render() {
-        const { description, urls } = this.props.image;
-        return(<div>
-            <img
-            ref={this.imageRef}             
-            alt={description} 
-            src={urls.regular}
-            />
-        </div>);
-    }
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} alt={description} src={urls.regular} />
+      </div>
+    );
+  }
 }
 
 export default ImageCard;
-//commit
