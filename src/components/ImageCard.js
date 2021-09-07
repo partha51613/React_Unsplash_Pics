1be1d41ed6e143378b3ab25857b46940bab64d61
Partha <github@partha51613.com>
2021-09-07 22:33:31 +0530

    Final touches

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,9 +3,7 @@ import React from 'react';
 class ImageCard extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = { spans: 0 };
-
     this.imageRef = React.createRef();
   }
 
@@ -15,9 +13,7 @@ class ImageCard extends React.Component {
 
   setSpans = () => {
     const height = this.imageRef.current.clientHeight;
-
     const spans = Math.ceil(height / 10);
-
     this.setState({ spans });
   };
 
